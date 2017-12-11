099814e40f780a4f583c44103a545656a61fda15
Marcus <mboperator@gmail.com>
2017-12-11 14:04:14 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,59 @@
-import React, { Component } from 'react';
-import { ToolHeader, Tabs } from '@procore/core-react';
-import logo from './logo.svg';
-import './App.css';
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>App React</div> at the bottom
+// of the page.
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ToolHeader>
-          <ToolHeader.Section>
-            <ToolHeader.Header>Thomas Fire Help</ToolHeader.Header>
-            <Tabs>
-              <Tabs.Tab>
-                <Tabs.Link>
-                  <a>About</a>
-                </Tabs.Link>
-              </Tabs.Tab>
-              <Tabs.Tab>
-                <Tabs.Link>
-                  <a>Supplies</a>
-                </Tabs.Link>
-              </Tabs.Tab>
-              <Tabs.Tab variant="active">
-                <Tabs.Link>
-                  <a>Volunteers</a>
-                </Tabs.Link>
-              </Tabs.Tab>
-              <Tabs.Tab>
-                <Tabs.Link>
-                  <a>Housing</a>
-                </Tabs.Link>
-              </Tabs.Tab>
-              <Tabs.Tab>
-                <Tabs.Link>
-                  <a>Resources</a>
-                </Tabs.Link>
-              </Tabs.Tab>
-            </Tabs>
-          </ToolHeader.Section>
-        </ToolHeader>
-      </div>
-    );
-  }
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import { Route, BrowserRouter } from 'react-router-dom'
+import styled from 'styled-components'
+
+import { ModuleProvider } from 'redux-modules';
+import { createStore } from 'redux';
+import { install, combineReducers } from 'redux-loop';
+
+import Home from './screens/Home'
+import LookingForHelp from './screens/LookingForHelp'
+import Helping from './screens/Helping'
+import Housing from './screens/Housing'
+import HousingForm from './screens/HousingForm'
+import Login from './screens/Login'
+import SignUp from './screens/SignUp'
+import './app.css'
+
+const store = createStore(s => s, {}, install());
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 98vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Montserrat, sans-serif;
+`
+
+const App = props => (
+  <BrowserRouter>
+    <ModuleProvider store={store} combineReducers={combineReducers}>
+      <AppContainer>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/looking_for_help" component={LookingForHelp} />
+        <Route exact path="/looking_for_help/housing" component={Housing} />
+        <Route exact path="/helping" component={Helping} />
+        <Route exact path="/helping/housing" component={HousingForm} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/sign_up" component={SignUp} />
+      </AppContainer>
+    </ModuleProvider>
+  </BrowserRouter>
+)
+
+App.defaultProps = {
+  name: 'David'
 }
 
-export default App;
+App.propTypes = {
+  name: PropTypes.string
+}
+
+export default App
