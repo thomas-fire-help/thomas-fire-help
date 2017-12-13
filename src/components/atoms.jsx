4f3288b38d7e874c4a61e68cb3f0780125a8aa7f
lnispel <lnispel@me.com>
2017-12-12 22:45:00 -0800

    Desktop Responsiveness

--- src/components/atoms.jsx
+++ src/components/atoms.jsx
@@ -6,23 +6,52 @@ const fadeIn = keyframes`
   to { opacity: 1; }
 `
 
-export const Container = styled.div`
+export const MobileContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  flex-wrap: wrap;
   animation: ${fadeIn} 1s forwards;
   opacity: 0;
+`
+export const SmallContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    animation: ${fadeIn} 1s forwards;
+    opacity: 0;
+    margin-left: 5%;
+    margin-right: 5%;
+`
+export const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    animation: ${fadeIn} 1s forwards;
+    opacity: 0;
+    margin-left: 10%;
+    margin-right: 10%;
+`
+
+export const LargeContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    animation: ${fadeIn} 1s forwards;
+    opacity: 0;
+    margin-left: 25%;
+    margin-right: 25%;
 `
 
 export const HeaderContainer = styled.div`
   align-self: flex-start;
   flex: 1;
+  flex: 0 1 100%;
   font-size: 24px;
   padding-bottom: 20px;
 `
 
 export const MobileHeaderContainer = styled.div`
   align-self: flex-start;
-  flex: .75;
   text-transform: uppercase;
   letter-spacing: 2px;
   width: 100%;
@@ -40,13 +69,14 @@ export const NavigationCard = styled.section`
   text-transform: uppercase;
   text-align: center;
 `
-
-export const Category = styled(Link)`
+export const LargeCategory = styled(Link)`
   color: #000;
   font-size: 18px;
   padding: 25px 25px;
   text-decoration: none;
-  margin-bottom: 20px;
+  margin: 10px;
+  margin-left: 20px;
+  width: 30%;
 
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 3px 0 rgba(0, 0, 0, 0.1);
   transition: box-shadow 0.2s ease-in-out;
@@ -65,12 +95,64 @@ export const Category = styled(Link)`
   }
 `
 
+export const SmallCategory = styled(Link)`
+    color: #000;
+    font-size: 18px;
+    padding: 25px 25px;
+    text-decoration: none;
+    margin: 10px;
+    margin-left: 10px;
+    width: 45%;
+
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 3px 0 rgba(0, 0, 0, 0.1);
+    transition: box-shadow 0.2s ease-in-out;
+    &:hover {
+      box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.23), 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+    }
+
+    h2 {
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      margin: 0px;
+    }
+
+    p {
+      color: grey;
+    }
+`
+
+export const Category = styled(Link)`
+  color: #000;
+  font-size: 18px;
+  padding: 25px 25px;
+  text-decoration: none;
+  margin: 20px;
+  margin-left: 20px;
+  width: 35%;
+
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 3px 0 rgba(0, 0, 0, 0.1);
+  transition: box-shadow 0.2s ease-in-out;
+  &:hover {
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.23), 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  }
+
+  h2 {
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    margin: 0px;
+  }
+
+  p {
+    color: grey;
+  }`
+
 export const MobileCategory = styled(Link)`
   color: #000;
   padding: 15px 15px;
   border: 1px solid black;
   text-decoration: none;
   margin-bottom: 20px;
+  width:100%;
 
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 3px 0 rgba(0, 0, 0, 0.1);
   transition: box-shadow 0.2s ease-in-out;
@@ -92,12 +174,13 @@ export const MobileCategory = styled(Link)`
   }
 `
 
-export const External = styled.a`
+export const LargeExternal = styled.a`
   color: #000;
   font-size: 18px;
   padding: 25px 25px;
   text-decoration: none;
-  margin-bottom: 20px;
+  margin: 10px;
+  width: 30%;
 
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 3px 0 rgba(0, 0, 0, 0.1);
   transition: box-shadow 0.2s ease-in-out;
@@ -115,6 +198,57 @@ export const External = styled.a`
     color: grey;
   }
 `
+export const SmallExternal = styled.a`
+    color: #000;
+    font-size: 18px;
+    padding: 25px 25px;
+    text-decoration: none;
+    margin: 10px;
+    margin-left: 10px;
+    width: 45%;
+
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 3px 0 rgba(0, 0, 0, 0.1);
+    transition: box-shadow 0.2s ease-in-out;
+    &:hover {
+      box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.23), 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+    }
+
+    h2 {
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      margin: 0px;
+    }
+
+    p {
+      color: grey;
+    }
+`
+
+export const External = styled.a`
+    color: #000;
+    font-size: 18px;
+    padding: 25px 25px;
+    text-decoration: none;
+    margin: 20px;
+    margin-left: 20px;
+    width: 35%;
+
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 3px 0 rgba(0, 0, 0, 0.1);
+    transition: box-shadow 0.2s ease-in-out;
+    &:hover {
+      box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.23), 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+    }
+
+    h2 {
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      margin: 0px;
+    }
+
+    p {
+      color: grey;
+    }
+`
 
 export const MobileExternal = styled.a`
   color: #000;
@@ -122,6 +256,7 @@ export const MobileExternal = styled.a`
   padding: 15px 15px;
   text-decoration: none;
   margin-bottom: 20px;
+  width:100%;
 
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 3px 0 rgba(0, 0, 0, 0.1);
   transition: box-shadow 0.2s ease-in-out;
--- src/screens/LookingForHelp.jsx
+++ src/screens/LookingForHelp.jsx
@@ -4,19 +4,27 @@ import styled from 'styled-components'
 import Layout from '../components/Layout'
 import MediaQuery from 'react-responsive';
 import {
+  MobileContainer,
+  SmallContainer,
   Container,
+  LargeContainer,
+  MobileExternal,
+  SmallExternal,
+  External,
+  LargeExternal,
+  MobileCategory,
+  SmallCategory,
+  Category,
+  LargeCategory,
+  ActionHolder,
   MobileHeaderContainer,
   HeaderContainer,
-  MobileExternal,
-  External,
-  MobileCategory,
-  Category,
 } from '../components/atoms'
 
 const Home = ({ history: { goBack }}) => (
   <Layout onBack={goBack}>
-    <Container>
-      <MediaQuery minDeviceWidth={320} maxDeviceWidth={480}>
+    <MobileContainer>
+      <MediaQuery maxWidth={480}>
         <MobileHeaderContainer style={{ marginBottom: '20px', textAlign: 'left' }}>
           <h1> What do you need? </h1>
         </MobileHeaderContainer>
@@ -39,31 +47,111 @@ const Home = ({ history: { goBack }}) => (
           <p> Free food, tools, housewares, animal supplies, and more. </p>
         </MobileExternal>
       </MediaQuery>
+    </MobileContainer>
 
-      <MediaQuery minDeviceWidth={481}>
-        <HeaderContainer>
-          <h1> What do you need? </h1>
-        </HeaderContainer>
-        <Category to="/looking_for_resources/volunteers">
-          <h2> 🛠 Volunteers </h2>
-          <p> For property assistance, events, animal care, and more. </p>
-        </Category>
-        <External
-          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=1117680778"
-          target="_blank"
-        >
-          <h2> 🏘 Housing </h2>
-          <p> View housing that is available now. </p>
-        </External>
-        <External
-          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
-          target="_blank"
-        >
-          <h2> 🗃 Supplies </h2>
-          <p> Free food, tools, housewares, animal supplies, and more. </p>
-        </External>
+    <SmallContainer>
+      <MediaQuery maxWidth={700} minWidth={480}>
+          <HeaderContainer>
+            <h1> What do you need? </h1>
+          </HeaderContainer>
+          <SmallCategory to="/looking_for_resources/volunteers">
+            <h3><div>🛠</div><div>Volunteers</div></h3>
+            <p> For property assistance, events, animal care, and more. </p>
+          </SmallCategory>
+          <SmallExternal
+            href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=1117680778"
+            target="_blank"
+          >
+            <h3><div>🏘</div><div>Housing</div></h3>
+            <p> View housing that is available now. </p>
+          </SmallExternal>
+          <SmallExternal
+            href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
+            target="_blank"
+          >
+            <h3><div>🗃</div><div>Supplies </div></h3>
+            <p> Free food, tools, housewares, animal supplies, and more. </p>
+          </SmallExternal>
+      </MediaQuery>
+    </SmallContainer>
+
+    <SmallContainer>
+      <MediaQuery minWidth={700} maxWidth={1042}>
+          <HeaderContainer>
+            <h1> What do you need? </h1>
+          </HeaderContainer>
+          <SmallCategory to="/looking_for_resources/volunteers">
+            <h3><div>🛠</div><div>Volunteers</div></h3>
+            <p> For property assistance, events, animal care, and more. </p>
+          </SmallCategory>
+          <SmallExternal
+            href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=1117680778"
+            target="_blank"
+          >
+            <h3><div>🏘</div><div>Housing</div></h3>
+            <p> View housing that is available now. </p>
+          </SmallExternal>
+          <SmallExternal
+            href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
+            target="_blank"
+          >
+            <h3><div>🗃</div><div>Supplies </div></h3>
+            <p> Free food, tools, housewares, animal supplies, and more. </p>
+          </SmallExternal>
+      </MediaQuery>
+    </SmallContainer>
+
+    <Container>
+      <MediaQuery minWidth={1042} maxWidth={1615}>
+          <HeaderContainer>
+            <h1> What do you need? </h1>
+          </HeaderContainer>
+          <Category to="/looking_for_resources/volunteers">
+            <h2><div>🛠</div><div>Volunteers</div></h2>
+            <p> For property assistance, events, animal care, and more. </p>
+          </Category>
+          <External
+            href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=1117680778"
+            target="_blank"
+          >
+            <h2><div>🏘</div><div>Housing</div></h2>
+            <p> View housing that is available now. </p>
+          </External>
+          <External
+            href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
+            target="_blank"
+          >
+            <h2><div>🗃</div><div>Supplies </div></h2>
+            <p> Free food, tools, housewares, animal supplies, and more. </p>
+          </External>
       </MediaQuery>
     </Container>
+
+    <LargeContainer>
+      <MediaQuery minWidth={1615}>
+          <HeaderContainer>
+            <h1> What do you need? </h1>
+          </HeaderContainer>
+          <LargeCategory to="/looking_for_resources/volunteers">
+            <h2><div>🛠</div><div>Volunteers</div></h2>
+            <p> For property assistance, events, animal care, and more. </p>
+          </LargeCategory>
+          <LargeExternal
+            href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=1117680778"
+            target="_blank"
+          >
+            <h2><div>🏘</div><div>Housing</div></h2>
+            <p> View housing that is available now. </p>
+          </LargeExternal>
+          <LargeExternal
+            href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
+            target="_blank"
+          >
+            <h2><div>🗃</div><div>Supplies </div></h2>
+            <p> Free food, tools, housewares, animal supplies, and more. </p>
+          </LargeExternal>
+      </MediaQuery>
+    </LargeContainer>
   </Layout>
 )
 
