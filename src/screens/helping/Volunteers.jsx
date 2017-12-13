1a17836c99248e89f3c066ea2c4b79250d379f52
Jeremy Yip <jeremy.yip7@gmail.com>
2017-12-12 19:23:08 -0800

    Edit mobile view for "I want to help"

--- src/screens/helping/Volunteers.jsx
+++ src/screens/helping/Volunteers.jsx
@@ -2,34 +2,55 @@ import React from 'react'
 import { connectModule } from 'redux-modules'
 import { Spinner } from '@procore/core-react'
 import { compose, lifecycle } from 'recompose'
+import MediaQuery from 'react-responsive'
 import housingModule from '../../modules/housing'
-import { Container, HeaderContainer, External } from '../../components/atoms'
+import {
+  Container,
+  MobileHeaderContainer,
+  HeaderContainer,
+  MobileExternal,
+  External
+} from '../../components/atoms'
 import Layout from '../../components/Layout'
 
 const Housing = ({ loading, data, history: { goBack }}) => (
   <Layout header="Housing" onBack={goBack}>
     <Container>
-      <HeaderContainer>
-        <h1>
-          I want to help...
-        </h1>
-      </HeaderContainer>
-      <External
-        href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=255593871"
-        target="_blank"
-      >
-        <h2>
-          🏩 An Organization
-        </h2>
-      </External>
-      <External
-        href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=1607302293"
-        target="_blank"
-      >
-        <h2>
-          🏡 My Neighbors
-        </h2>
-      </External>
+      <MediaQuery minDeviceWidth={320} maxDeviceWidth={480}>
+        <MobileHeaderContainer style={{ marginBottom: '20px', textAlign: 'left' }}>
+          <h1> I want to help... </h1>
+        </MobileHeaderContainer>
+        <MobileExternal
+          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=255593871"
+          target="_blank"
+        >
+          <h2> 🏩 An Organization </h2>
+        </MobileExternal>
+        <MobileExternal
+          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=1607302293"
+          target="_blank"
+        >
+          <h2> 🏡 My Neighbors </h2>
+        </MobileExternal>
+      </MediaQuery>
+
+      <MediaQuery minDeviceWidth={481}>
+        <HeaderContainer>
+          <h1> I want to help... </h1>
+        </HeaderContainer>
+        <External
+          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=255593871"
+          target="_blank"
+        >
+          <h2> 🏩 An Organization </h2>
+        </External>
+        <External
+          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=1607302293"
+          target="_blank"
+        >
+          <h2> 🏡 My Neighbors </h2>
+        </External>
+      </MediaQuery>
     </Container>
   </Layout>
 )
