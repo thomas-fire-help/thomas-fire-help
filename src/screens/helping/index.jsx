97a5d163755cfcb7f87ca3160e855a6e087aa3fa
Ian Nicholson <ipnicholson@gmail.com>
2017-12-13 18:40:59 -0800

    whoops

--- src/screens/helping/index.jsx
+++ src/screens/helping/index.jsx
@@ -115,29 +115,9 @@ const Home = ({ history: { goBack }}) => (
           target="_blank"
         >
           <h2> 🍲 Offer Food </h2>
-          <p> I have food or meals to offer. </p>
+          <p> Donate food or meals. </p>
         </External>
-
-
-
-        <Category to="/helping/volunteers">
-          <h2> 🛠 Volunteer </h2>
-          <p> With individuals or organizations. </p>
-        </Category>
-        <External
-          href="https://docs.google.com/forms/d/1W4Z7wwVGVBVNuf9bajq3ud0UwtkrrQ4H6AwY5F155X0/edit"
-          target="_blank"
-        >
-          <h2> 🏡 Offer Housing </h2>
-          <p> Help a displaced family, short term or long term. </p>
-        </External>
-        <External
-          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
-          target="_blank"
-        >
-          <h2> 🗃 Offer Supplies </h2>
-          <p> Donate supplies or register a drop-off location. </p>
-        </External>
+        
       </MediaQuery>
     </Container>
   </Layout>
