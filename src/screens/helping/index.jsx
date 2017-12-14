92c51e08635e92b28b978fc668b01a487db0e87e
Ian Nicholson <ipnicholson@gmail.com>
2017-12-13 18:35:25 -0800

    add desktop navcards, still need updated links

--- src/screens/helping/index.jsx
+++ src/screens/helping/index.jsx
@@ -72,6 +72,54 @@ const Home = ({ history: { goBack }}) => (
         <HeaderContainer>
           <h1> How do you want to help? </h1>
         </HeaderContainer>
+
+        <External
+          href="https://docs.google.com/forms/d/1W4Z7wwVGVBVNuf9bajq3ud0UwtkrrQ4H6AwY5F155X0/edit"
+          target="_blank"
+        >
+          <h2> 🏡 Offer Housing </h2>
+          <p> Help a displaced family, short term or long term. </p>
+        </External>
+
+        <External
+          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
+          target="_blank"
+        >
+          <h2> 🐴 Help With Animals </h2>
+          <p> Volunteer to help animals. </p>
+        </External>
+        
+        <Category to="/helping/volunteers">
+          <h2> 🛠 Volunteer </h2>
+          <p> With individuals or organizations. </p>
+        </Category>
+
+        <External
+          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
+          target="_blank"
+        >
+          <h2> 👕 Donate or Collect Supplies </h2>
+          <p> Donate supplies or register a drop-off location. </p>
+        </External>
+
+        <External
+          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
+          target="_blank"
+        >
+          <h2> ⬇️ Drop-Off Supplies </h2>
+          <p> View registered drop-off locations. </p>
+        </External>
+
+        <External
+          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
+          target="_blank"
+        >
+          <h2> 🍲 Offer Food </h2>
+          <p> I have food or meals to offer. </p>
+        </External>
+
+
+
         <Category to="/helping/volunteers">
           <h2> 🛠 Volunteer </h2>
           <p> With individuals or organizations. </p>
