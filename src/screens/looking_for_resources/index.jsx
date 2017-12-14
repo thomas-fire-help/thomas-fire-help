909e9fe6343533f950560a88743f22cf12b534ea
Ian Nicholson <ipnicholson@gmail.com>
2017-12-13 18:17:35 -0800

    add desktop navcards

--- src/screens/looking_for_resources/index.jsx
+++ src/screens/looking_for_resources/index.jsx
@@ -28,7 +28,7 @@ const Home = ({ history: { goBack }}) => (
           <h2> 🏘 Housing </h2>
           <p> View housing that is available now. </p>
         </MobileExternal>
-        
+
         <MobileCategory to="/looking_for_resources/volunteers">
           <h2> 🛠 Volunteers </h2>
           <p> For property assistance, events, and more. </p>
@@ -41,6 +41,7 @@ const Home = ({ history: { goBack }}) => (
           <h2> 🐴 Help With Animals </h2>
           <p> Animal supplies & volunteer help. </p>
         </MobileExternal>
+
         <MobileExternal
           href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
           target="_blank"
@@ -48,6 +49,7 @@ const Home = ({ history: { goBack }}) => (
           <h2> 👕 Supplies </h2>
           <p> Free clothing, tools, housewares, and more. </p>
         </MobileExternal>
+
         <MobileExternal
           href="https://drive.google.com/open?id=1Ws8tgwwf6q9I9enknXBtL3sEZf7orBE_v5OBwhZtVw4"
           target="_blank"
@@ -55,6 +57,7 @@ const Home = ({ history: { goBack }}) => (
           <h2> 🍲 Food </h2>
           <p> Something free to eat. </p>
         </MobileExternal>
+        
         <MobileExternal
           href="https://drive.google.com/open?id=1Oi1ij6GmdOVR9WGqL_LGycydMsSF--HKGzDQyM-7WxU"
           target="_blank"
@@ -68,10 +71,7 @@ const Home = ({ history: { goBack }}) => (
         <HeaderContainer>
           <h1> What do you need? </h1>
         </HeaderContainer>
-        <Category to="/looking_for_resources/volunteers">
-          <h2> 🛠 Volunteers </h2>
-          <p> For property assistance, events, animal care, and more. </p>
-        </Category>
+
         <External
           href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=1117680778"
           target="_blank"
@@ -79,13 +79,44 @@ const Home = ({ history: { goBack }}) => (
           <h2> 🏘 Housing </h2>
           <p> View housing that is available now. </p>
         </External>
+
+        <Category to="/looking_for_resources/volunteers">
+          <h2> 🛠 Volunteers </h2>
+          <p> For property assistance, events, and more. </p>
+        </Category>
+
+        <External
+          href="https://drive.google.com/open?id=1hbUqWT0P2SkQtSLdfir0S-j2Iw8k1R-gNmIwXsXHlgE"
+          target="_blank"
+        >
+          <h2> 🐴 Help With Animals </h2>
+          <p> Animal supplies & volunteer help. </p>
+        </External>
+
         <External
           href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
           target="_blank"
         >
-          <h2> 🗃 Supplies </h2>
-          <p> Free food, tools, housewares, animal supplies, and more. </p>
+          <h2> 👕 Supplies </h2>
+          <p> Free clothing, tools, housewares, and more. </p>
         </External>
+
+        <External
+          href="https://drive.google.com/open?id=1Ws8tgwwf6q9I9enknXBtL3sEZf7orBE_v5OBwhZtVw4"
+          target="_blank"
+        >
+          <h2> 🍲 Food </h2>
+          <p> Something free to eat. </p>
+        </External>
+        
+        <External
+          href="https://drive.google.com/open?id=1Oi1ij6GmdOVR9WGqL_LGycydMsSF--HKGzDQyM-7WxU"
+          target="_blank"
+        >
+          <h2> ⛑ Services </h2>
+          <p> FEMA assistance, disaster recovery & more. </p>
+        </External>
+
       </MediaQuery>
     </Container>
   </Layout>
