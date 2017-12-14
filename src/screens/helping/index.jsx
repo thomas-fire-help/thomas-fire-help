07029154b76df8084754df8757aecf61571c39dc
Ian Nicholson <ipnicholson@gmail.com>
2017-12-13 18:34:00 -0800

    add navcards for mobile

--- src/screens/helping/index.jsx
+++ src/screens/helping/index.jsx
@@ -20,10 +20,7 @@ const Home = ({ history: { goBack }}) => (
         <MobileHeaderContainer style={{ whiteSpace: 'normal', overflow: 'auto', marginBottom: '20px', textAlign: 'left' }}>
           <h1> How do you want to help? </h1>
         </MobileHeaderContainer>
-        <MobileCategory to="/helping/volunteers">
-          <h2> 🛠 Volunteer </h2>
-          <p> With individuals or organizations. </p>
-        </MobileCategory>
+
         <MobileExternal
           href="https://docs.google.com/forms/d/1W4Z7wwVGVBVNuf9bajq3ud0UwtkrrQ4H6AwY5F155X0/edit"
           target="_blank"
@@ -31,13 +28,44 @@ const Home = ({ history: { goBack }}) => (
           <h2> 🏡 Offer Housing </h2>
           <p> Help a displaced family, short term or long term. </p>
         </MobileExternal>
+
         <MobileExternal
           href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
           target="_blank"
         >
-          <h2> 🗃 Offer Supplies </h2>
+          <h2> 🐴 Help With Animals </h2>
+          <p> Volunteer to help animals. </p>
+        </MobileExternal>
+        
+        <MobileCategory to="/helping/volunteers">
+          <h2> 🛠 Volunteer </h2>
+          <p> With individuals or organizations. </p>
+        </MobileCategory>
+
+        <MobileExternal
+          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
+          target="_blank"
+        >
+          <h2> 👕 Donate or Collect Supplies </h2>
           <p> Donate supplies or register a drop-off location. </p>
         </MobileExternal>
+
+        <MobileExternal
+          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
+          target="_blank"
+        >
+          <h2> ⬇️ Drop-Off Supplies </h2>
+          <p> View registered drop-off locations. </p>
+        </MobileExternal>
+
+        <MobileExternal
+          href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
+          target="_blank"
+        >
+          <h2> 🍲 Offer Food </h2>
+          <p> I have food or meals to offer. </p>
+        </MobileExternal>
+
       </MediaQuery>
 
       <MediaQuery minDeviceWidth={481}>
