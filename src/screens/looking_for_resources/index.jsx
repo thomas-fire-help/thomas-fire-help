1949e03d03edc90392abaf126ade3e7e3cddb51f
Ian Nicholson <ipnicholson@gmail.com>
2017-12-13 18:13:58 -0800

    add nav cards for mobile

--- src/screens/looking_for_resources/index.jsx
+++ src/screens/looking_for_resources/index.jsx
@@ -20,10 +20,7 @@ const Home = ({ history: { goBack }}) => (
         <MobileHeaderContainer style={{ marginBottom: '20px', textAlign: 'left' }}>
           <h1> What do you need? </h1>
         </MobileHeaderContainer>
-        <MobileCategory to="/looking_for_resources/volunteers">
-          <h2> 🛠 Volunteers </h2>
-          <p> For property assistance, events, animal care, and more. </p>
-        </MobileCategory>
+
         <MobileExternal
           href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=1117680778"
           target="_blank"
@@ -31,12 +28,39 @@ const Home = ({ history: { goBack }}) => (
           <h2> 🏘 Housing </h2>
           <p> View housing that is available now. </p>
         </MobileExternal>
+        
+        <MobileCategory to="/looking_for_resources/volunteers">
+          <h2> 🛠 Volunteers </h2>
+          <p> For property assistance, events, and more. </p>
+        </MobileCategory>
+
+        <MobileExternal
+          href="https://drive.google.com/open?id=1hbUqWT0P2SkQtSLdfir0S-j2Iw8k1R-gNmIwXsXHlgE"
+          target="_blank"
+        >
+          <h2> 🐴 Help With Animals </h2>
+          <p> Animal supplies & volunteer help. </p>
+        </MobileExternal>
         <MobileExternal
           href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
           target="_blank"
         >
-          <h2> 🗃 Supplies </h2>
-          <p> Free food, tools, housewares, animal supplies, and more. </p>
+          <h2> 👕 Supplies </h2>
+          <p> Free clothing, tools, housewares, and more. </p>
+        </MobileExternal>
+        <MobileExternal
+          href="https://drive.google.com/open?id=1Ws8tgwwf6q9I9enknXBtL3sEZf7orBE_v5OBwhZtVw4"
+          target="_blank"
+        >
+          <h2> 🍲 Food </h2>
+          <p> Something free to eat. </p>
+        </MobileExternal>
+        <MobileExternal
+          href="https://drive.google.com/open?id=1Oi1ij6GmdOVR9WGqL_LGycydMsSF--HKGzDQyM-7WxU"
+          target="_blank"
+        >
+          <h2> ⛑ Services </h2>
+          <p> FEMA assistance, disaster recovery & more. </p>
         </MobileExternal>
       </MediaQuery>
 
