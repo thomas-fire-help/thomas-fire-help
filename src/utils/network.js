f15b764163abbc05818100e21638e5c721b3243c
Marcus Bernales <marcusbernales@gmail.com>
2017-12-21 18:57:39 -0800

    Added ENV variable for selecting the correct API endpoint

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,11 +1,6 @@
 export const getHost = () => {
-  if (process.env.NODE_ENV === 'development') {
-    return 'https://firehelp-api-staging.herokuapp.com'
-  }
-  if (process.env.NODE_ENV === 'staging') {
-    return 'https://firehelp-api-staging.herokuapp.com'
-  }
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.REACT_APP_API_HOST === 'production') {
     return 'https://firehelp-api.herokuapp.com'
   }
+  return 'https://firehelp-api-staging.herokuapp.com'
 }
