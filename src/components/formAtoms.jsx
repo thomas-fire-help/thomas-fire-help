a8c7b3ebf136755d7aa9ce484d3f863cb6b551ca
Ian Nicholson <ipnicholson@gmail.com>
2017-12-29 18:29:22 -0800

    initial commit

---
+++ src/components/formAtoms.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const RequiredIndicator = styled.em`
+  color: red;
+`
+
+export const Label = styled.div`
+  font-size: 18px;
+  text-transform: uppercase;
+  padding: 15px 0;
+`
+export const StackContainer = styled.div`
+  margin: 0 0;
+`
+
+export const FormSection = styled.div`
+  padding: 30px 0;
+`
